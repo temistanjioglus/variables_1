1d8269cec493e75a243adf06c67e0f3aa47b5700
jordi temistanjioglus <>
2020-06-16 16:59:37 +0200

    no message

---
+++ DEclaracion_operaciones.java
@@ -0,0 +1,23 @@
+
+public class DEclaracion_operaciones {
+
+	public static void main(String[] args) {
+		
+		String nom = "Jordi";
+		
+		String cognom1 = "Temistanjioglus";
+		
+		String cognom2 = "San Jose";
+		
+		int dia = 10;
+		
+		int any = 2020;
+		
+		int mes = 06;
+	
+	
+		System.out.println (cognom1+" "+cognom2+" "+nom);
+		System.out.println(dia+"/"+mes+"/"+any);
+	}
+
+}
